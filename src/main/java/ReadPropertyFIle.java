6f6d0d69d54475fb469aa5110f579bfca6a8c4a4
uriwin <uriwin@gmail.com>
2020-03-26 15:48:04 +0200

    removed comment

--- src/main/java/ReadPropertyFIle.java
+++ src/main/java/ReadPropertyFIle.java
@@ -7,12 +7,10 @@ public class ReadPropertyFIle {
     public Properties ReadConfigFile() throws IOException {
         String propFileName = "config.properties";
         try (InputStream input = this.getClass().getClassLoader().getResourceAsStream(propFileName)) {
-
             if (input == null) {
                 throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
             }
             Properties prop = new Properties();
-            //load a properties file from class path, inside static method
             prop.load(input);
             return prop;
         }
